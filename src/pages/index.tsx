7a9698dbd07569a7923b537c9bad2654134db3b6
GalerDekstop <gabigaler50@gmail.com>
2022-08-23 11:20:56 +0300

    Add resume to public resources

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaTwitter, FaFilePdf } from "react-icons/fa";
 
 const Home: NextPage = () => {
   return (
@@ -46,41 +46,78 @@ const Home: NextPage = () => {
             <a className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">
               Read more...
             </a>
-          </Link>{" "}
+          </Link>
+          {/* <div
+            style={{ position: "relative", paddingBottom: "56.25%", height: 0 }}
+          >
+            <iframe
+              src="https://www.loom.com/embed/af321ba5b4404d5787703a6e01bb209e"
+              frameBorder={0}
+              webkitallowfullscreen=""
+              mozallowfullscreen=""
+              allowFullScreen=""
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+              }}
+            />
+          </div> */}
         </li>
       </ul>
 
       <hr className="border-t-2 border-t-slate-50 w-1/4 my-2"></hr>
 
-      <h1>Socials</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://twitter.com/gabigaler7"
-          >
-            <FaTwitter className="inline mr-1" />
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreferrer" href="https://github.com/galer7">
-            <FaGithub className="inline mr-1" />
-            GitHub
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/gabriel-galer-83098b163"
-          >
-            <FaLinkedin className="inline mr-1" />
-            Linkedin
-          </a>{" "}
-        </li>
-      </ul>
+      <div className="flex justify-evenly w-1/4">
+        <div>
+          <h1>Socials</h1>
+          <ul>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://twitter.com/gabigaler7"
+              >
+                <FaTwitter className="inline mr-1" />
+                Twitter
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/galer7"
+              >
+                <FaGithub className="inline mr-1" />
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/gabriel-galer-83098b163"
+              >
+                <FaLinkedin className="inline mr-1" />
+                Linkedin
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1>Resources</h1>
+          <ul>
+            <li>
+              <a target="_blank" rel="noreferrer" href="Galer_Gabriel_CV.pdf">
+                <FaFilePdf className="inline mr-1 text-white" />
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </main>
   );
 };
