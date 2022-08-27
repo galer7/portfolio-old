39788ad99fc2f426eac672a7a5c0fb176465f648
GalerDekstop <gabigaler50@gmail.com>
2022-08-27 14:12:16 +0300

    Grammarly check

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -13,7 +13,7 @@ export default function Image({
   return (
     <div className={`ml-[calc(-50vw_+_50%)] relative w-screen ${h}`}>
       <Link href={src}>
-        <a target="_blank">
+        <a target="_blank" className={h}>
           <NextImage src={src} layout="fill" objectFit="contain" alt={alt} />
         </a>
       </Link>
