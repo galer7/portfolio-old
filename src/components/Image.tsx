83dfe903a9d80675e7292703d6cd22f0fe5bb94f
GalerDekstop <gabigaler50@gmail.com>
2022-08-27 14:29:44 +0300

    Remove dynamic built classnames for tailwind

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,19 +1,11 @@
 import NextImage from "next/image";
 import Link from "next/link";
 
-export default function Image({
-  src,
-  h,
-  alt,
-}: {
-  src: string;
-  h: string;
-  alt: string;
-}) {
+export default function Image({ src, alt }: { src: string; alt: string }) {
   return (
-    <div className={`ml-[calc(-50vw_+_50%)] relative w-screen ${h}`}>
+    <div className={`left-[calc(-50vw_+_50%)] relative w-screen h-96`}>
       <Link href={src}>
-        <a target="_blank" className={h}>
+        <a target="_blank">
           <NextImage src={src} layout="fill" objectFit="contain" alt={alt} />
         </a>
       </Link>
